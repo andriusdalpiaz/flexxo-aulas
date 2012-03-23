f9e188a456464d8e2092037d64f6ab069db7c683
Marcos Vinicius de C. Marcon <marconvcm@gmail.com>
2012-03-23 19:16:47 -0300

    atualizacao

---
+++ turma-mercanet/oo/aula01/aula_oo_01/aula_oo_01/Mesa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aula_oo_01
+{
+    class Mesa
+    {
+    }
+}
---
+++ turma-mercanet/oo/aula01/aula_oo_01/aula_oo_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aula_oo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesa minhaLindaMesa;
+            minhaLindaMesa = new Mesa();
+            minhaLindaMesa.ToString();
+        }
+    }
+}
