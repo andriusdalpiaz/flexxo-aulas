42f0479fc2c33e0c0c0f50b8788f6f314b040b57
Marcos Vinicius de C. Marcon <marconvcm@gmail.com>
2012-03-24 16:07:55 -0300

    atualizacao

---
+++ turma-mercanet/oo/aula03.1/aula_oo_031/aula_oo_031/Curso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aula_oo_031
+{
+    class Curso
+    {
+        public Curso()
+        {
+            Console.Write("Informe o nome do curso: ");
+            nome = Console.ReadLine();
+            Console.Write("Informe a carga horária: ");
+            cargaHoraria = int.Parse(Console.ReadLine());
+        }
+
+        public void MostraDados()
+        {
+            Console.WriteLine("Nome: " + nome + " Carga Horaria: " + cargaHoraria);
+        }
+
+        public string nome;
+        public int cargaHoraria;
+    }
+}
---
+++ turma-mercanet/oo/aula03.1/aula_oo_031/aula_oo_031/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aula_oo_031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso[] cursos = new Curso[2];
+
+            Console.WriteLine("Cadastro de cursos: \n");
+
+            for (int i = 0; i < cursos.Length; i++)
+            {
+                cursos[i] = new Curso();
+            }
+
+            for (int i = 0; i < cursos.Length; i++)
+            {
+                cursos[i].MostraDados();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
