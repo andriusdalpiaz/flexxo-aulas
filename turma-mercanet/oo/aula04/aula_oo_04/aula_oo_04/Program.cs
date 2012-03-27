ea56235d032715d493ffce8514621522b1be8f30
Marcos Vinicius de C. Marcon <marconvcm@gmail.com>
2012-03-26 21:06:41 -0300

    atualizacao

--- turma-mercanet/oo/aula04/aula_oo_04/aula_oo_04/Program.cs
+++ turma-mercanet/oo/aula04/aula_oo_04/aula_oo_04/Program.cs
@@ -9,6 +9,23 @@ namespace aula_oo_04
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iniciando");
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Telefone: ");
+                ITelefone telefone = GeraTelefone();
+                telefone.Apertar(Console.ReadKey().KeyChar);
+            }
+        }
+
+        public static ITelefone GeraTelefone() 
+        {
+            Random random = new Random();
+
+            if ((random.Next(5) % 2) == 0)
+                return new TelefoneSimples();
+            else
+                return new TelefoneJorgeForman();
         }
     }
 }
