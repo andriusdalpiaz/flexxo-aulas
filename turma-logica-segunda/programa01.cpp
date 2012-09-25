597890a67fba332eac1c5627b3ca444ea6bfa01d
Marcos Vinicius Marcon <marconvcm@gmail.com>
2012-09-24 20:51:35 -0700

    alteracao

---
+++ turma-logica-segunda/programa01.cpp
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "conio.h"
+
+int a, b, c;
+
+int main(){
+    
+    a = 5;
+    b = 15;
+    c = 0;
+    
+    c = a * b;
+    
+    printf("%d", c);
+    
+    getch();    
+}
+
+
+
+
---
+++ turma-logica-segunda/programa02.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "conio.h"
+
+int a, b, c;
+
+int main(){
+    scanf("%d", &a);
+    scanf("%d", &b);        
+    c = a * b;
+    printf("%d", c);
+    getch();
+}
